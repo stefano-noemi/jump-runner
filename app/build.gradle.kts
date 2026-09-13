478a1f8a9cb8b8740576169f06b422888752f40b
stefano-noemi <stompanatostefano8@gmail.com>
2026-09-13 10:31:54 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,11 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.example.jumprunner"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.example.jumprunner"
         minSdk = 23
@@ -12,7 +14,17 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
